14ee11e177dc64f86b2ab43491bf5749288211c6
unknown <patrickkeita80@yahoo.com>
2024-05-15 05:31:15 -0700

    Update productList.mjs

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,7 +1,7 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
-    return `<li class="product-card">
+  return `<li class="product-card">
       <a href="product_pages/index.html?product=${product.id}">
         <img src="${product.image}" alt="Image of ${product.name}">
         <h3 class="card__brand">${product.brand}</h3>
@@ -9,27 +9,23 @@ function productCardTemplate(product) {
         <p class="product-card__price">$${product.price}</p>
       </a>
     </li>`;
-  }
+}
 
 export default class ProductListing {
-    constructor(category, dataSource, listElement) {
-        this.category = category;
-        this.dataSource = dataSource;
-        this.listElement = listElement;
-    };
-  
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
 
-    async init() {
-        // our dataSource will return a Promise...so we can use await to resolve it.
-        const list = await this.dataSource.getData();
-        // render the list - to be completed
-        this.renderList(list);
-    } 
-    
-    renderList(list) {
-        renderListWithTemplate(productCardTemplate, this.list.Element, list);
-    }
-      
-};
+  async init() {
+    // our dataSource will return a Promise...so we can use await to resolve it.
+    const list = await this.dataSource.getData(this.category);
+    // render the list - to be completed
+    this.renderList(list);
+  }
 
-
+  renderList(list) {
+    renderListWithTemplate(productCardTemplate, this.list.Element, list);
+  }
+}
