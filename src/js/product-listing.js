bab0ae6b4f46b9b5693d9bdb104ff4971701df69
unknown <patrickkeita80@yahoo.com>
2024-05-15 05:07:40 -0700

    Add product-listing.js

---
+++ src/js/product-listing.js
@@ -0,0 +1,15 @@
+import ProductData from "./ProductData.mjs";
+import ProductListing from "./ProductList.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+
+loadHeaderFooter();
+
+const category = getParam("category");
+// first create an instance of our ProductData class.
+const dataSource = new ProductData();
+// then get the element we want the product list to render in
+const listElement = document.querySelector(".product-list");
+// then create an instance of our ProductList class and send it the correct information.
+const productListing = new ProductListing(category, dataSource, listElement);
+
+productListing.init();
