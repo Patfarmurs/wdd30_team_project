468d673cc0ebb1814766f4fe7175b56120201647
unknown <patrickkeita80@yahoo.com>
2024-05-19 00:03:41 -0700

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,3 @@
-import ProductData from "./ProductData.mjs";
-
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
-
-// Create an instance of ProductListing & product data
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-
-
