82e5cf837e94f7ebd58009bc67de09ebf1d145b8
unknown <patrickkeita80@yahoo.com>
2024-05-20 08:33:55 -0700

    Update checkoutProcess.mjs

--- src/js/checkoutProcess.mjs
+++ src/js/checkoutProcess.mjs
@@ -1,4 +1,4 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, alertMessage, removeAllAlerts, } from "./utils.mjs";
 import ExternalServices from "./ExternalServices.mjs";
 
 const services = new ExternalServices();
@@ -85,10 +85,18 @@ export default class CheckoutProcess {
     json.items = packageItems(this.list);
     console.log(json);
     try {
-      const res = await services.checkout(json);
-      console.log(res);
-    } catch (err) {
-      console.log(err);
+        const res = await services.checkout(json);
+        console.log(res);
+        setLocalStorage("so-cart", []);
+        location.assign("/checkout/success.html");
+      } catch (err) {
+        // get rid of any preexisting alerts.
+        removeAllAlerts();
+        for (let message in err.message) {
+          alertMessage(err.message[message]);
+        }
+  
+        console.log(err);
+      }
     }
-  }
-}
+  };
