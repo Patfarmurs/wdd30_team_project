c3ae9ca33631bb27e12af87e31caab6b5a3c6649
unknown <patrickkeita80@yahoo.com>
2024-05-30 23:47:48 -0700

    Update product-listing.js

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,15 +1,11 @@
-import ExternalServices from "./ExternalServices.mjs";
-import ProductListing from "./ProductList.mjs";
 import { loadHeaderFooter, getParam } from "./utils.mjs";
+import ExternalServices from "./ExternalServices.mjs";
+import ProductList from "./ProductList.mjs";
 
 loadHeaderFooter();
-
 const category = getParam("category");
-// first create an instance of our ProductData class.
 const dataSource = new ExternalServices();
-// then get the element we want the product list to render in
-const listElement = document.querySelector(".product-list");
-// then create an instance of our ProductList class and send it the correct information.
-const productListing = new ProductListing(category, dataSource, listElement);
+const element = document.querySelector(".product-list");
+const listing = new ProductList(category, dataSource, element);
 
-productListing.init();
+listing.init();
